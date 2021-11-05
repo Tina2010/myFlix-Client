fcf0526681594ee4690d05a4ba785a9cdbbeac07
Tina2010 <t.rittmueller@gmx.de>
2021-11-05 09:13:46 +0100

    [remove] movie props

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-import { Link } from "react-router-dom";
+import Link from "react-router-dom/Link";
 
 import './movie-card.scss';
 
@@ -29,10 +28,3 @@ export class MovieCard extends React.Component {
     ); 
   }
 }
-
-MovieCard.prototype = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};
